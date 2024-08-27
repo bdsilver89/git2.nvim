88bdd9152284c4b08c987087674faaa08a425fbe
Brian Silver <github@briansilver.net>
2024-08-27 11:48:27 -0400

    ci: update doc generation and fix lint error

--- lua/git2/config.lua
+++ lua/git2/config.lua
@@ -4,14 +4,14 @@ local M = {}
 
 ---@return Git2Config
 function M.default_values()
-	return {}
+  return {}
 end
 
 M.values = M.default_values()
 
 ---@param opts  Git2Config?
 function M.setup(opts)
-	M.values = vim.tbl_deep_extend("force", M.values, opts or {})
+  M.values = vim.tbl_deep_extend("force", M.values, opts or {})
 end
 
 return M
